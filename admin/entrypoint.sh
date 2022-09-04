cf1eb8fbbf1cdc253750da1272c4bb4f31d7cd13
ziminyuri <zimin.yuri22@gmail.com>
2022-09-04 21:40:09 +0700

    fix for docker

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -7,5 +7,7 @@ python manage.py createsuperuser --username admin --email test@test.com --noinpu
 
 echo "Start gunicorn server"
 gunicorn -w 1 config.wsgi:application --bind 0.0.0.0:8000
-celery -A config -b "amqp://rabbitmq:5672/vhost" beat --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A config  worker --loglevel=info
+celery -A config  beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+#celery -A config -b "amqp://rabbitmq:5672/vhost" beat --scheduler django_celery_beat.schedulers:DatabaseScheduler
 exec "$@"
